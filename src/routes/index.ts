81a6c735cd4c44ccc212fa648e115bdd45fb8345
Fergus <fergus.jingtai@gmail.com>
2024-05-04 20:10:14 +0800

    feat: add /users/signup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import usersV1 from "./v1/users";
 import tasksV1 from "./v1/tasks";
 
 const router = express();
 
+router.use("/v1/users", usersV1);
 router.use("/v1/tasks", tasksV1);
 
 export default router;
