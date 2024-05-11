41e1443dff3b5d9367fff141222dd8a156b5f6e2
Lucy <lucy70381@kimo.com>
2024-05-12 05:37:40 +0800

    refactor: rename validateMiddleware to validateRequest

--- src/routes/v1/upload.ts
+++ src/routes/v1/upload.ts
@@ -2,11 +2,11 @@ import express from 'express';
 
 import { uploadImage } from '@controllers/upload';
 import upload from '@middlewares/image';
-import { validateMiddleware } from '@middlewares/validatorMiddleware';
+import { validateRequest } from '@middlewares/validateRequest';
 import { uploadImageRequestSchema } from '@schema/upload';
 
 const router = express.Router();
 
-router.post('/image', upload.single('file'), validateMiddleware(uploadImageRequestSchema), uploadImage);
+router.post('/image', upload.single('file'), validateRequest(uploadImageRequestSchema), uploadImage);
 
 export default router;
