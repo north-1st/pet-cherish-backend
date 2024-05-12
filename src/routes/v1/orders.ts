765111f06b81a540665d3e4de75c12786a2b22c0
Joanna Chen <joannachenchencom@gmail.com>
2024-05-12 22:01:36 +0800

    feat: swagger docs for API/createOrder

--- src/routes/v1/orders.ts
+++ src/routes/v1/orders.ts
@@ -15,7 +15,19 @@ const {
 
 const router = Router();
 // 之後要補 User 登入檢查的 middleware，還要同時回傳 user_id 給 controller 使用
-router.post('/', createOrder);
+router.post(
+  '/',
+  /*  #swagger.parameters['body'] = {
+          in: 'body',
+          description: '建立訂單',
+          schema: { 
+            user_id: "'663fd8ce5eeb13779e3a2f76'", 
+            task_id: "456" 
+          }
+      } 
+  */
+  createOrder
+);
 router.patch('/:order_id/refuse-sitter', updateOrdersByRefuseSitter);
 router.patch('/:order_id/accept-sitter', updateOrdersByAcceptSitter);
 router.patch('/:order_id/paid', updateOrdersByPaid);
