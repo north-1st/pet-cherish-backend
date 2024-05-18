9e6a5bd215dcf7a05a53205cca767f8047f64ce4
Joanna Chen <joannachenchencom@gmail.com>
2024-05-18 23:11:06 +0800

    feat: sitters swagger setting

--- src/routes/v1/sitters.ts
+++ src/routes/v1/sitters.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 
 import { applySitter, getSitterService, sitterApprove, sitterReject, updateSitterService } from '@controllers/sitters';
+import * as s from '@middlewares/swaggers/sitters';
 import { validateRequest } from '@middlewares/validateRequest';
 import { applySitterRequestSchema, sitterRequestSchema, updateSitterServiceRequestSchema } from '@schema/sitter';
 
 const router = express.Router();
 
-router.post('/apply-sitter', validateRequest(applySitterRequestSchema), applySitter);
-router.get('/sitters/:user_id', validateRequest(sitterRequestSchema), getSitterService);
-router.patch('/sitters', validateRequest(updateSitterServiceRequestSchema), updateSitterService);
+router.post('/apply-sitter', validateRequest(applySitterRequestSchema), applySitter, s.applySitter);
+router.get('/sitters/:user_id', validateRequest(sitterRequestSchema), getSitterService, s.getSitterService);
+router.patch('/sitters', validateRequest(updateSitterServiceRequestSchema), updateSitterService, s.updateSitterService);
 
-router.patch('/sitters/:user_id/approve', validateRequest(sitterRequestSchema), sitterApprove);
-router.patch('/sitters/:user_id/reject', validateRequest(sitterRequestSchema), sitterReject);
+router.patch('/sitters/:user_id/approve', validateRequest(sitterRequestSchema), sitterApprove, s.sitterApprove);
+router.patch('/sitters/:user_id/reject', validateRequest(sitterRequestSchema), sitterReject, s.sitterReject);
 
 export default router;
