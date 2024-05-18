690c0214854e3c39e4183ea5454ff34f25911df9
Joanna Chen <joannachenchencom@gmail.com>
2024-05-18 22:41:14 +0800

    feat: pet swagger setting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,6 @@
 import express from 'express';
 
-import {
-  petsMiddleware,
-  sitterMiddleware,
-  tasksMiddleware,
-  uploadMiddleware,
-  usersMiddleware,
-} from '@middlewares/tagMiddleware';
+import { sitterMiddleware, tasksMiddleware, uploadMiddleware, usersMiddleware } from '@middlewares/tagMiddleware';
 import ordersV1 from '@routes/v1/orders';
 import petsV1 from '@routes/v1/pets';
 import sitterV1 from '@routes/v1/sitters';
@@ -20,7 +14,7 @@ router.use('/v1/orders', ordersV1);
 router.use('/v1/upload', uploadMiddleware, uploadV1);
 router.use('/v1/users', usersMiddleware, usersV1);
 router.use('/v1', sitterMiddleware, sitterV1);
-router.use('/v1', petsMiddleware, petsV1);
+router.use('/v1', petsV1);
 router.use('/v1/tasks', tasksMiddleware, tasksV1);
 
 export default router;
--- src/routes/v1/pets.ts
+++ src/routes/v1/pets.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
 import { createPet, getPets, updatePet } from '@controllers/pets';
+import * as s from '@middlewares/swaggers/pets';
 import { validateRequest } from '@middlewares/validateRequest';
 import { createPetRequestSchema, getPetRequestSchema, updatePetRequestSchema } from '@schema/pet';
 
 const router = Router();
 
-router.post('/users/:user_id/pets', validateRequest(createPetRequestSchema), createPet);
-router.get('/users/:user_id/pets', validateRequest(getPetRequestSchema), getPets);
-router.patch('/pets/:pet_id', validateRequest(updatePetRequestSchema), updatePet);
+router.post('/users/:user_id/pets', validateRequest(createPetRequestSchema), createPet, s.createPet);
+router.get('/users/:user_id/pets', validateRequest(getPetRequestSchema), getPets, s.getPets);
+router.patch('/pets/:pet_id', validateRequest(updatePetRequestSchema), updatePet, s.updatePet);
 
 export default router;
