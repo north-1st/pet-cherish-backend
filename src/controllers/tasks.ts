66dc948c5c77e70671b89ea228fea5aea9d084bd
Joanna Chen <joannachenchencom@gmail.com>
2024-05-18 20:45:38 +0800

    refactor: API/createReview

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -21,17 +21,75 @@ export const createReview = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const { order_id } = req.params;
-    const { user_id, rating, content } = req.body;
+  const { order_id } = req.params;
+  const { user_id, rating, content } = req.body;
 
+  const handleOwnerReview = async (task_id: string, sitter_user_id: string) => {
+    try {
+      const result = await prisma.$transaction(async (prisma) => {
+        // (1) 飼主撰寫評價：
+        const newReview = await prisma.review.create({
+          data: {
+            task_id,
+            pet_owner_user_id: user_id,
+            pet_owner_rating: rating,
+            pet_owner_content: content,
+            sitter_user_id,
+          },
+        });
+
+        // (2) Task 掛 review_id
+        await prisma.task.update({
+          where: {
+            id: task_id,
+          },
+          data: {
+            review_id: newReview.id,
+          },
+        });
+
+        // (3) 更新保姆 Sitter: average_rating, total_reviews
+        // - 重新計算給保姆所有評價平均分：
+        const newAverageRating = await prisma.review.aggregate({
+          where: {
+            sitter_user_id,
+          },
+          _avg: {
+            pet_owner_rating: true,
+          },
+        });
+        // - 取得最新總計：
+        const newTotalReviews = await prisma.sitter.count({
+          where: {
+            user_id: sitter_user_id,
+          },
+        });
+
+        await prisma.sitter.update({
+          where: {
+            user_id: sitter_user_id,
+          },
+          data: {
+            average_rating: newAverageRating._avg.pet_owner_rating || 0,
+            total_reviews: newTotalReviews,
+          },
+        });
+      });
+
+      return result;
+    } catch (error) {
+      console.log('handleReviewByOwner error: ', error);
+      throw error;
+    }
+  };
+
+  try {
     // 找到指定訂單
     const targetOrder = await prisma.order.findUnique({
       where: {
         id: order_id,
       },
     });
-
     if (!targetOrder) {
       res.status(404).json({
         message: 'Order is not found!',
@@ -45,7 +103,6 @@ export const createReview = async (
         id: targetOrder.task_id,
       },
     });
-
     if (!targetTask) {
       res.status(404).json({
         message: 'Task is not found!',
@@ -55,28 +112,9 @@ export const createReview = async (
     }
 
     if (targetTask.user_id === user_id) {
-      // 飼主撰寫評價：
-      const newReview = await prisma.review.create({
-        data: {
-          task_id: targetTask.id,
-          pet_owner_user_id: user_id,
-          pet_owner_rating: rating,
-          pet_owner_content: content,
-          sitter_user_id: targetOrder.sitter_user_id,
-        },
-      });
-
-      // Task 掛 review_id
-      await prisma.task.update({
-        where: {
-          id: targetTask.id,
-        },
-        data: {
-          review_id: newReview.id,
-        },
-      });
+      handleOwnerReview(targetTask.id, targetOrder.sitter_user_id);
     } else {
-      // 保姆撰寫評價：
+      // (1) 保姆撰寫評價：
       await prisma.review.update({
         where: {
           task_id: targetTask.id,
@@ -85,6 +123,34 @@ export const createReview = async (
           sitter_rating: rating,
           sitter_content: content,
           sitter_user_created_at: new Date(),
+        },
+      });
+
+      // (2) 更新飼主 User: average_rating, total_reviews
+
+      // - 重新計算給飼主所有評價平均分：
+      const newAverageRating = await prisma.review.aggregate({
+        where: {
+          pet_owner_user_id: targetOrder.pet_owner_user_id,
+        },
+        _avg: {
+          sitter_rating: true,
+        },
+      });
+      // - 取得最新總計：
+      const newTotalReviews = await prisma.user.count({
+        where: {
+          id: targetOrder.pet_owner_user_id,
+        },
+      });
+
+      await prisma.user.update({
+        where: {
+          id: targetOrder.sitter_user_id,
+        },
+        data: {
+          average_rating: newAverageRating._avg.sitter_rating || 0,
+          total_reviews: newTotalReviews,
         },
       });
     }
