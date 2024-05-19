1c52f2e14cb4a3d87d717c4eea972eea8aa57060
Joanna Chen <joannachenchencom@gmail.com>
2024-05-19 14:35:02 +0800

    refactor: API/createReview

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -16,116 +16,6 @@ export const getTasks: RequestHandler = async (req, res, next) => {
   }
 };
 
-const handleOwnerReview = async (task_id: string, sitter_user_id: string, req_body: ReviewRequest) => {
-  const { user_id, rating, content } = req_body;
-
-  try {
-    await prisma.$transaction(async (prisma) => {
-      // (1) 飼主撰寫評價：
-      const newReview = await prisma.review.create({
-        data: {
-          task_id,
-          pet_owner_user_id: user_id,
-          pet_owner_rating: rating,
-          pet_owner_content: content,
-          sitter_user_id,
-        },
-      });
-
-      // (2) Task 掛 review_id
-      await prisma.task.update({
-        where: {
-          id: task_id,
-        },
-        data: {
-          review_id: newReview.id,
-        },
-      });
-
-      // 重新計算[保姆]所有評價平均分：
-      const newAverageRating = await prisma.review.aggregate({
-        where: {
-          sitter_user_id,
-        },
-        _avg: {
-          pet_owner_rating: true,
-        },
-      });
-      // 取得[保姆]評價最新總計：
-      const newTotalReviews = await prisma.sitter.count({
-        where: {
-          user_id: sitter_user_id,
-        },
-      });
-
-      // (3) 更新[保姆] Sitter: average_rating, total_reviews
-      await prisma.sitter.update({
-        where: {
-          user_id: sitter_user_id,
-        },
-        data: {
-          average_rating: newAverageRating._avg.pet_owner_rating || 0,
-          total_reviews: newTotalReviews,
-        },
-      });
-    });
-  } catch (error) {
-    console.log('handleReviewByOwner error: ', error);
-    throw error;
-  }
-};
-
-const handleSitterReview = async (task_id: string, pet_owner_user_id: string, req_body: ReviewRequest) => {
-  const { rating, content } = req_body;
-
-  try {
-    await prisma.$transaction(async (prisma) => {
-      // (1) 保姆撰寫評價：
-      await prisma.review.update({
-        where: {
-          task_id,
-        },
-        data: {
-          sitter_rating: rating,
-          sitter_content: content,
-          sitter_user_created_at: new Date(),
-        },
-      });
-
-      // 重新計算[飼主]所有評價平均分：
-      const newAverageRating = await prisma.review.aggregate({
-        where: {
-          pet_owner_user_id,
-        },
-        _avg: {
-          sitter_rating: true,
-        },
-      });
-
-      // 取得[飼主]評價最新總計：
-      const newTotalReviews = await prisma.user.count({
-        where: {
-          id: pet_owner_user_id,
-        },
-      });
-
-      // (2) 更新[飼主] User: average_rating, total_reviews
-      await prisma.user.update({
-        where: {
-          id: pet_owner_user_id,
-        },
-        data: {
-          average_rating: newAverageRating._avg.sitter_rating || 0,
-          total_reviews: newTotalReviews,
-        },
-      });
-    });
-  } catch (error) {
-    console.log('handleSitterReview error: ', error);
-    throw error;
-  }
-};
-
 export const createReview = async (
   req: Request<OrdersParams, unknown, ReviewRequest>,
   res: Response,
@@ -133,6 +23,116 @@ export const createReview = async (
 ) => {
   const { order_id } = req.params;
   const { user_id, rating, content } = req.body;
+
+  const handleOwnerReview = async (task_id: string, sitter_user_id: string, req_body: ReviewRequest) => {
+    const { user_id, rating, content } = req_body;
+
+    try {
+      await prisma.$transaction(async (prisma) => {
+        // (1) 飼主撰寫評價：
+        const newReview = await prisma.review.create({
+          data: {
+            task_id,
+            pet_owner_user_id: user_id,
+            pet_owner_rating: rating,
+            pet_owner_content: content,
+            sitter_user_id,
+          },
+        });
+
+        // (2) Task 掛 review_id
+        await prisma.task.update({
+          where: {
+            id: task_id,
+          },
+          data: {
+            review_id: newReview.id,
+          },
+        });
+
+        // 重新計算[保姆]所有評價平均分：
+        const newAverageRating = await prisma.review.aggregate({
+          where: {
+            sitter_user_id,
+          },
+          _avg: {
+            pet_owner_rating: true,
+          },
+        });
+        // 取得[保姆]評價最新總計：
+        const newTotalReviews = await prisma.sitter.count({
+          where: {
+            user_id: sitter_user_id,
+          },
+        });
+
+        // (3) 更新[保姆] Sitter: average_rating, total_reviews
+        await prisma.sitter.update({
+          where: {
+            user_id: sitter_user_id,
+          },
+          data: {
+            average_rating: newAverageRating._avg.pet_owner_rating || 0,
+            total_reviews: newTotalReviews,
+          },
+        });
+      });
+    } catch (error) {
+      console.log('handleReviewByOwner error: ', error);
+      throw error;
+    }
+  };
+
+  const handleSitterReview = async (task_id: string, pet_owner_user_id: string, req_body: ReviewRequest) => {
+    const { rating, content } = req_body;
+
+    try {
+      await prisma.$transaction(async (prisma) => {
+        // (1) 保姆撰寫評價：
+        await prisma.review.update({
+          where: {
+            task_id,
+          },
+          data: {
+            sitter_rating: rating,
+            sitter_content: content,
+            sitter_user_created_at: new Date(),
+          },
+        });
+
+        // 重新計算[飼主]所有評價平均分：
+        const newAverageRating = await prisma.review.aggregate({
+          where: {
+            pet_owner_user_id,
+          },
+          _avg: {
+            sitter_rating: true,
+          },
+        });
+
+        // 取得[飼主]評價最新總計：
+        const newTotalReviews = await prisma.user.count({
+          where: {
+            id: pet_owner_user_id,
+          },
+        });
+
+        // (2) 更新[飼主] User: average_rating, total_reviews
+        await prisma.user.update({
+          where: {
+            id: pet_owner_user_id,
+          },
+          data: {
+            average_rating: newAverageRating._avg.sitter_rating || 0,
+            total_reviews: newTotalReviews,
+          },
+        });
+      });
+    } catch (error) {
+      console.log('handleSitterReview error: ', error);
+      throw error;
+    }
+  };
 
   try {
     // 找到指定訂單
