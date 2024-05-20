79d3c001415a0951d049d1b9f5f6b22a04dbf8ca
Lucy <lucy70381@kimo.com>
2024-05-20 23:51:20 +0800

    feat: add tasks swagger

--- src/routes/v1/tasks.ts
+++ src/routes/v1/tasks.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import * as TasksController from '@controllers/tasks';
-import { createTask, deleteTask, getUserTasks, updateTask } from '@controllers/tasks';
+import { createTask, deleteTask, getTasksByUser, updateTask } from '@controllers/tasks';
 import requiresAuth from '@middlewares/requiresAuth';
 import * as s from '@middlewares/swaggers/tasks';
 import { validateRequest } from '@middlewares/validateRequest';
@@ -17,7 +17,7 @@ const router = express.Router();
 router.post('/tasks', requiresAuth, requiresAuth, validateRequest(createTaskRequestSchema), createTask);
 router.patch('/tasks/:task_id', requiresAuth, validateRequest(updateTaskRequestSchema), updateTask);
 router.delete('/tasks/:task_id', requiresAuth, validateRequest(deleteTaskRequestSchema), deleteTask);
-router.get('/users/:user_id/tasks', validateRequest(getTasksByUserRequestSchema), getUserTasks);
+router.get('/users/:user_id/tasks', validateRequest(getTasksByUserRequestSchema), getTasksByUser);
 router.post('/:task_id/review', TasksController.createReview, s.createReview);
 
 export default router;
--- src/schema/task.ts
+++ src/schema/task.ts
@@ -1,22 +1,42 @@
 import { z } from 'zod';
 
+import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
 import { ServiceType, TaskPublic, TaskStatus } from '@prisma/client';
 import { objectIdSchema } from '@schema/objectId';
 import { paginationRequestSchema } from '@schema/pagination';
 
-export const createTaskBodySchema = z.object({
-  title: z.string(),
-  public: z.enum([TaskPublic.OPEN, TaskPublic.CLOSED]),
-  service_type: z.nativeEnum(ServiceType),
-  city: z.string(),
-  district: z.string(),
-  unit_price: z.number(),
-  detail: z.string(),
-  accept_sitter_contact: z.boolean(),
-  start_at: z.string().datetime(),
-  end_at: z.string().datetime(),
-  pet_id: objectIdSchema,
-});
+extendZodWithOpenApi(z);
+
+export const createTaskBodySchema = z
+  .object({
+    title: z.string(),
+    public: z.enum([TaskPublic.OPEN, TaskPublic.CLOSED]),
+    service_type: z.nativeEnum(ServiceType),
+    city: z.string(),
+    district: z.string(),
+    unit_price: z.number(),
+    detail: z.string(),
+    accept_sitter_contact: z.boolean(),
+    start_at: z.string().datetime(),
+    end_at: z.string().datetime(),
+    pet_id: objectIdSchema,
+  })
+  .openapi({
+    description: '建立任務',
+    example: {
+      title: '任務名稱',
+      public: 'OPEN',
+      service_type: 'WALKING',
+      city: '台北市',
+      district: '中正區',
+      unit_price: 200,
+      detail: 'detail',
+      accept_sitter_contact: true,
+      start_at: '2022-01-01T00:00:00.000Z',
+      end_at: '2022-01-01T03:00:00.000Z',
+      pet_id: '66489faba8bcd77a22dedbe3',
+    },
+  });
 
 export const createTaskRequestSchema = z.object({
   body: createTaskBodySchema,
@@ -48,23 +68,62 @@ export const deleteTaskRequestSchema = z.object({
   }),
 });
 
-export const taskResponseSchema = z.object({
-  id: objectIdSchema,
-  title: z.string(),
-  public: z.nativeEnum(TaskPublic).default(TaskPublic.OPEN),
-  status: z.nativeEnum(TaskStatus).default(TaskStatus.NULL),
-  service_type: z.nativeEnum(ServiceType),
-  city: z.string(),
-  district: z.string(),
-  unit_price: z.number(),
-  total: z.number(),
-  detail: z.string(),
-  accept_sitter_contact: z.boolean().default(false),
-  start_at: z.string().datetime(),
-  end_at: z.string().datetime(),
-  created_at: z.string().datetime(),
-  updated_at: z.string().datetime(),
-});
+export const taskResponseSchema = z
+  .object({
+    id: objectIdSchema,
+    title: z.string(),
+    public: z.nativeEnum(TaskPublic).default(TaskPublic.OPEN),
+    status: z.nativeEnum(TaskStatus).default(TaskStatus.NULL),
+    service_type: z.nativeEnum(ServiceType),
+    city: z.string(),
+    district: z.string(),
+    unit_price: z.number(),
+    total: z.number(),
+    detail: z.string(),
+    accept_sitter_contact: z.boolean().default(false),
+    start_at: z.string().datetime(),
+    end_at: z.string().datetime(),
+    created_at: z.string().datetime(),
+    updated_at: z.string().datetime(),
+  })
+  .openapi({
+    examples: [
+      {
+        id: '66489faba8bcd77a22dedbe3',
+        title: '任務名稱',
+        public: 'OPEN',
+        status: 'NULL',
+        service_type: 'WALKING',
+        city: '台北市',
+        district: '中正區',
+        unit_price: 200,
+        total: 1,
+        detail: 'detail',
+        accept_sitter_contact: true,
+        start_at: '2022-01-01T00:00:00.000Z',
+        end_at: '2022-01-01T03:00:00.000Z',
+        created_at: '2022-01-01T00:00:00.000Z',
+        updated_at: '2022-01-01T00:00:00.000Z',
+      },
+      {
+        id: '66489faba8bcd77a22dedbe3',
+        title: '任務名稱',
+        public: 'CLOSED',
+        status: 'NULL',
+        service_type: 'WALKING',
+        city: '台北市',
+        district: '中正區',
+        unit_price: 200,
+        total: 1,
+        detail: 'detail',
+        accept_sitter_contact: true,
+        start_at: '2022-01-01T00:00:00.000Z',
+        end_at: '2022-01-01T03:00:00.000Z',
+        created_at: '2022-01-01T00:00:00.000Z',
+        updated_at: '2022-01-01T00:00:00.000Z',
+      },
+    ],
+  });
 
 export const reviewBodySchema = z.object({
   user_id: z.string(),
