2816df9be14b24c814ec45f2386016722779c1eb
Lucy <lucy70381@kimo.com>
2024-05-26 01:29:33 +0800

    fix: wrong filter in isExistingSitter

--- src/middlewares/isExistingSitter.ts
+++ src/middlewares/isExistingSitter.ts
@@ -5,17 +5,24 @@ import prisma from '@prisma';
 import { UserBaseSchema } from '@schema/user';
 
 const isExistingSitter = async (req: Request<UserBaseSchema>, _res: Response, next: NextFunction) => {
-  const sitter = await prisma.sitter.findUnique({
-    where: {
-      id: req.params.user_id,
-    },
-  });
+  try {
+    const sitter = await prisma.sitter.findUnique({
+      where: {
+        user_id: req.params.user_id,
+        user: {
+          is_sitter: true,
+        },
+      },
+    });
 
-  if (!sitter) {
-    throw createHttpError(404, 'Sitter not found');
+    if (!sitter) {
+      throw createHttpError(404, 'Sitter not found');
+    }
+
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  next();
 };
 
 export default isExistingSitter;
