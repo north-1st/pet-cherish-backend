10684e4ab168df316f64ca561974c61c4f292a6b
Joanna Chen <joannachenchencom@gmail.com>
2024-05-25 14:20:29 +0800

    refactor: swagger/updateOrders

--- src/config/swaggers/orders.ts
+++ src/config/swaggers/orders.ts
@@ -1,20 +1,51 @@
 import { z } from 'zod';
 
-import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import { OpenAPIRegistry, RouteConfig } from '@asteasolutions/zod-to-openapi';
 import { BearerAuth } from '@schema/bearerAuth';
 import { orderBodySchema } from '@schema/orders';
 import { userBaseSchema } from '@schema/user';
 
 export const setOrdersSwagger = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
   createOrder(registry, bearerAuth);
-  //   refuseSitter(registry, bearerAuth);
-  //   acceptSitter(registry, bearerAuth);
-  //   payForOrder(registry, bearerAuth);
-  //   completeOrder(registry, bearerAuth);
-  //   cancelOrder(registry, bearerAuth);
+  refuseSitter(registry, bearerAuth);
+  acceptSitter(registry, bearerAuth);
+  payForOrder(registry, bearerAuth);
+  completeOrder(registry, bearerAuth);
+  cancelOrder(registry, bearerAuth);
   //   getPetOwnerOrders(registry);
   //   getSitterOrders(registry);
 };
+
+const commonOderSetting = (bearerAuth: BearerAuth): RouteConfig => ({
+  method: 'patch',
+  tags: ['Orders'],
+  path: '',
+  security: [{ [bearerAuth.name]: [] }],
+  summary: '',
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: orderBodySchema,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'Update Successfully!',
+    },
+    401: {
+      description: 'Unauthorized',
+    },
+    403: {
+      description: 'Forbidden',
+    },
+    404: {
+      description: 'Order is not found!',
+    },
+  },
+});
 
 const createOrder = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
   registry.registerPath({
@@ -49,5 +80,45 @@ const createOrder = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
         description: 'Order has been created!',
       },
     },
+  });
+};
+
+const refuseSitter = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  registry.registerPath({
+    ...commonOderSetting(bearerAuth),
+    path: '/api/v1/orders/{order_id}/refuse-sitter',
+    summary: '更新：訂單狀態 / 拒絕指定保母',
+  });
+};
+
+const acceptSitter = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  registry.registerPath({
+    ...commonOderSetting(bearerAuth),
+    path: '/api/v1/orders/{order_id}/accept-sitter',
+    summary: '更新：訂單狀態 / 接受指定保母',
+  });
+};
+
+const payForOrder = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  registry.registerPath({
+    ...commonOderSetting(bearerAuth),
+    path: '/api/v1/orders/{order_id}/paid',
+    summary: '更新：訂單狀態 / 飼主付款',
+  });
+};
+
+const completeOrder = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  registry.registerPath({
+    ...commonOderSetting(bearerAuth),
+    path: '/api/v1/orders/{order_id}/complete',
+    summary: '更新：訂單狀態 / 任務完成',
+  });
+};
+
+const cancelOrder = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  registry.registerPath({
+    ...commonOderSetting(bearerAuth),
+    path: '/api/v1/orders/{order_id}/cancel',
+    summary: '更新：訂單狀態 / 取消訂單',
   });
 };
