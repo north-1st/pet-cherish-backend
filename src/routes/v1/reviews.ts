d2825b7875c3bd88443b2db38356c2ddcadbbbd1
Lucy <lucy70381@kimo.com>
2024-06-10 02:50:56 +0800

    feat: update middleware for get reviews

--- src/routes/v1/reviews.ts
+++ src/routes/v1/reviews.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import { createReview, getOwnerReviews, getReviewByTaskId, getSitterReviews, updateReview } from '@controllers/reviews';
+import isExistingSitter from '@middlewares/isExistingSitter';
+import isExistingUser from '@middlewares/isExistingUser';
 import requiresAuth from '@middlewares/requiresAuth';
 import { validateRequest } from '@middlewares/validateRequest';
 import { getReviewRequestSchema, getReviewsParamSchema, reviewRequestSchema } from '@schema/review';
 
 const router = Router();
-router.get('/pet-owners/:user_id/reviews', validateRequest(getReviewsParamSchema), getOwnerReviews);
-router.get('/sitters/:user_id/reviews', validateRequest(getReviewsParamSchema), getSitterReviews);
+router.get('/pet-owners/:user_id/reviews', validateRequest(getReviewsParamSchema), isExistingUser, getOwnerReviews);
+router.get('/sitters/:user_id/reviews', validateRequest(getReviewsParamSchema), isExistingSitter, getSitterReviews);
 router
   .route('/tasks/:task_id/review')
   .get(validateRequest(getReviewRequestSchema), getReviewByTaskId)
