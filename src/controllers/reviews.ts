72fc38d31a40123bad4376544ea9bdbc1d526b29
Joanna Chen <joannachenchencom@gmail.com>
2024-06-09 17:27:43 +0800

    refactor: API/reviews

--- src/controllers/reviews.ts
+++ src/controllers/reviews.ts
@@ -81,6 +81,7 @@ export const createReview = async (req: Request, res: Response, next: NextFuncti
             sitter_rating: rating,
             sitter_content: content,
             sitter_user_created_at: new Date(),
+            sitter_user_updated_at: new Date(),
           },
         });
 
@@ -181,6 +182,14 @@ export const updateReview = async (req: Request, res: Response, next: NextFuncti
       return;
     }
 
+    if (targetOrder.pet_owner_user_id !== req.user.id && targetOrder.sitter_user_id !== req.user.id) {
+      res.status(403).json({
+        message: 'Forbidden!',
+        status: false,
+      });
+      return;
+    }
+
     if (targetOrder.pet_owner_user_id === req.user.id) {
       // 飼主更新評價
       await prisma.review.update({
@@ -190,6 +199,7 @@ export const updateReview = async (req: Request, res: Response, next: NextFuncti
         data: {
           pet_owner_rating: rating,
           pet_owner_content: content,
+          pet_owner_updated_at: new Date(),
         },
       });
     } else {
@@ -201,6 +211,7 @@ export const updateReview = async (req: Request, res: Response, next: NextFuncti
         data: {
           sitter_rating: rating,
           sitter_content: content,
+          sitter_user_updated_at: new Date(),
         },
       });
     }
@@ -221,6 +232,39 @@ export const getReviewByTaskId = async (req: Request, res: Response, next: NextF
     const targetReview = await prisma.review.findUnique({
       where: {
         task_id,
+      },
+      select: {
+        pet_owner_rating: true,
+        pet_owner_content: true,
+        pet_owner_updated_at: true,
+        sitter_rating: true,
+        sitter_content: true,
+        sitter_user_updated_at: true,
+        pet_owner: {
+          select: {
+            id: true,
+            email: true,
+            real_name: true,
+            nickname: true,
+            avatar: true,
+          },
+        },
+        sitter: {
+          select: {
+            id: true,
+            email: true,
+            real_name: true,
+            nickname: true,
+            avatar: true,
+          },
+        },
+        task: {
+          select: {
+            id: true,
+            title: true,
+            service_type: true,
+          },
+        },
       },
     });
     if (!targetReview) {
@@ -255,7 +299,7 @@ export const getOwnerReviews = async (req: Request<UserBaseSchema>, res: Respons
             id: true,
             sitter_rating: true,
             sitter_content: true,
-            sitter_user_created_at: true,
+            sitter_user_updated_at: true,
             sitter: {
               select: {
                 id: true,
@@ -300,8 +344,8 @@ export const getSitterReviews = async (req: Request<UserBaseSchema>, res: Respon
             id: true,
             pet_owner_rating: true,
             pet_owner_content: true,
-            pet_owner_created_at: true,
-            owner: {
+            pet_owner_updated_at: true,
+            pet_owner: {
               select: {
                 id: true,
                 email: true,
