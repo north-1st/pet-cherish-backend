0ee6ce97d8b3d501680310cc97594017c4d342d5
atsao1 <atsao1@lenovo.com>
2024-06-11 01:17:14 +0800

    refactor: enhance

---
+++ src/config/swaggers/home.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+import { OpenAPIRegistry, RouteConfig } from '@asteasolutions/zod-to-openapi';
+import { BearerAuth } from '@schema/bearerAuth';
+import { homeResponseSchema } from '@schema/home';
+import { orderBodySchema, ordersResponseSchema } from '@schema/orders';
+import { userBaseSchema } from '@schema/user';
+
+export const setHomeSwagger = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  getHomeInfo(registry, bearerAuth);
+};
+
+const getHomeInfo = (registry: OpenAPIRegistry, bearerAuth: BearerAuth) => {
+  registry.registerPath({
+    method: 'get',
+    tags: ['Home'],
+    path: '/api/v1/home',
+    summary: '取得: 首頁資訊',
+    responses: {
+      200: {
+        description: 'Get homepage successfully',
+        content: {
+          'application/json': {
+            schema: z.array(homeResponseSchema),
+          },
+        },
+      },
+      400: {
+        description: 'Get homepage info failed',
+      },
+    },
+  });
+};
