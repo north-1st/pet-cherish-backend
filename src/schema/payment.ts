b13c06364cbbc181a375cdb1fd8140f73ce20197
Joanna Chen <joannachenchencom@gmail.com>
2024-06-23 17:57:06 +0800

    feat: payment swagger

--- src/schema/payment.ts
+++ src/schema/payment.ts
@@ -101,7 +101,25 @@ export const completeResponseSchema = z
       retrieve: z.object({
         id: z.string(),
         amount_total: z.number(),
-        metadata: z.object({}),
+        metadata: z.object({
+          order_id: z.string(),
+          task_id: z.string(),
+        }),
+        payment_status: z.enum(['paid', 'unpaid', 'no_payment_required']),
+        payment_intent: z.object({
+          amount: z.number(),
+          amount_received: z.number(),
+          created: z.number(), // 付款时间
+          payment_method: z.object({
+            type: z.string(),
+            card: z
+              .object({
+                brand: z.string(),
+                last4: z.string(),
+              })
+              .optional(),
+          }),
+        }),
       }),
       list_items: z.array(
         z.object({
@@ -123,10 +141,24 @@ export const completeResponseSchema = z
       message: 'Your payment was successful',
       data: {
         retrieve: {
-          id: 'cs_test_b1ZmurAFYdbEabofvi11fQrcrT5pwKrPNG04PBxL7YelDvX6byGdNajsOm',
-          amount_total: 80000,
+          id: 'cs_test_b1cr522PQ4qwypfN24f5UzrAbnuLUgAq7fXBB8bklCTrWu2wfgUmlQt2QQ',
+          amount_total: 305000,
           metadata: {
-            order_id: '123456',
+            order_id: '6677c51e52789c9282cb5504',
+            task_id: '6658a7d754390e6a3ed4370d',
+          },
+          payment_status: 'paid',
+          payment_intent: {
+            amount: 305000,
+            amount_received: 305000,
+            created: 1719125342, // 付款时间: Unix 时间
+            payment_method: {
+              type: 'card',
+              card: {
+                brand: 'visa',
+                last4: '4242',
+              },
+            },
           },
         },
         list_items: [
